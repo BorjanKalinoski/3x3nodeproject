e770d69eb7a498c39174431129687cb91437d630
Borjan Kalinoski <boki_borjan@hotmail.com>
2019-01-23 23:09:55 +0100

    uploading post just to db

--- controllers/posts/posts.js
+++ controllers/posts/posts.js
@@ -75,7 +75,7 @@ const uploadPOST = (req, res, db, moment) => {
             title: title,
             descr: descr,
             sdesc: sdesc,
-            post_date: post_date,
+            // post_date: post_date,
             mainimg: mainimg.name
         })
             .into('posts')
@@ -104,69 +104,6 @@ const uploadPOST = (req, res, db, moment) => {
 
     // console.log(req.files);
     return res.json(req.body).end();
-};
-const uploadPost = (req, res, db, moment) => {
-    const {title, sdesc, descr, mainimg, images, post_date} = req.body;
-    // console.log(req.body);
-    if (!moment(post_date).isValid()) {
-        console.log('dateerror');
-        res.status(400).json('Bad request');
-        return;
-    }
-    if (!title || !sdesc || !descr || !mainimg.name || images.length === 0) {
-        console.log('serror');
-        res.status(400).json('Bad request');
-        return;
-    }
-    if (mainimg.type.substr(0, 5) !== 'image') {
-        res.status(400).json('Bad request');
-        return;
-    }
-    if (getFileExtension(mainimg.name) === false) {
-        res.status(400).json('Bad request');
-        return;
-    }
-    let postimages = [];
-    for (let i in images) {
-        if (images[i].type.substr(0, 5) !== 'image') {
-            return res.status(400).json('Bad request');
-        }
-        if (getFileExtension(images[i].name) === false) {
-            return res.status(400).json('Bad request');
-        }
-        postimages.push(images[i].name.toLowerCase());
-    }
-
-    db.transaction(trx => {
-        const queries = [];
-        const main = trx.insert({
-            title: title,
-            sdesc: sdesc,
-            descr: descr,
-            mainimg: mainimg.name,
-            // post_date: post_date
-        })
-            .into('posts')
-            .returning('id')
-            .then(post_id => {
-                postimages.forEach(image => {
-                    trx('post_images')
-                        .insert({
-                            post_id: post_id[0],
-                            image: image
-                        }).then(response => {
-                        res.json(response[0]);
-                    });
-                });
-            });
-        queries.push(main);
-        Promise.all(queries).then(trx.commit)
-            .catch(trx.rollback)
-    }).catch(err => {
-        console.log(err);
-        res.status(400).json('Bad Request');
-    });
-    res.status(200).json('Post Submitted');
 };
 module.exports = {
     uploadPOST: uploadPOST,
