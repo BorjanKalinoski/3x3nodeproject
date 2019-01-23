e328c32a16cd4f22dbbee408476e34e574fdd941
Borjan Kalinoski <boki_borjan@hotmail.com>
2019-01-23 22:30:46 +0100

    uploading post just to db

--- controllers/posts/posts.js
+++ controllers/posts/posts.js
@@ -33,7 +33,9 @@ const uploadPOST = (req, res, db, moment) => {
     // console.log('FILES', req.files);
     // console.log('slikite', req.files);
     // console.log('IMAGES', images);
-    console.log('SLIKA E ', images);
+    console.log('SLIKA E ', req.files);
+    console.log('body', req.body);
+
 
     // console.log('aa', imag);
     // console.log('BODY', req.body);
