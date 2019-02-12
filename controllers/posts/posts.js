6717988569a410ae694682963215b7dfe02dce6c
Borjan Kalinoski <boki_borjan@hotmail.com>
2019-02-12 16:26:20 +0100

    Updating edit post

--- controllers/posts/posts.js
+++ controllers/posts/posts.js
@@ -112,8 +112,11 @@ const editPost = async (req, res, db, fs, S3FSImplementation) => {
                 throw err;
             });
             mi = mi[0].mainimg;
-            console.log('main image is ', mi);
-            let a= S3FSImplementation.createReadStream(mi);
+            console.log('main image is ', mi, 'path is', mainimage.path);
+            S3FSImplementation.unlink(mi, (err => {
+                throw err;
+            }));
+            let a = S3FSImplementation.createReadStream(mainimage.path).pipe(S3FSImplementation.createWriteStream(mi));
             a.on('finish',()=>{
                 console.log('yes');
                 console.log('aaaaaaa', a);
