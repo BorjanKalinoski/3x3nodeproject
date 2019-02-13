155cece6ddf526175a1d0644e9c361db5e51f6ea
Borjan Kalinoski <boki_borjan@hotmail.com>
2019-02-13 13:59:01 +0100

    Fully fixing Edit post

--- controllers/posts/posts.js
+++ controllers/posts/posts.js
@@ -198,7 +198,7 @@ const editPost = async (req, res, db, fs, S3FSImplementation) => {
                     ext = post_image.name.slice((post_image.name.lastIndexOf('.') - 1 >>> 0) + 2).toLowerCase();
                     console.log('ctr is', ctr);//ctr not updating for some reason?
                     let imgquery = await db('post_images').insert({
-                        image: `post_${id}_${ctr}.${ext}`,
+                        image: `post_${id}_img${ctr}.${ext}`,
                         post_id: id
                     })
                         .returning('*')
